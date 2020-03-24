1c40be3096b150ab042907e417c745d7f7d0f2b7
Sepehr Sohrabi <sepehr0sohrabi@gmail.com>
2020-03-24 21:02:50 +0430

    add requirements.txt and custom admin model

--- blog/models.py
+++ blog/models.py
@@ -16,9 +16,6 @@ class UserProfile(models.Model):
     avatar = models.FileField(upload_to='files/user_avatar/', null=False, blank=False, validators=[validate_file_extension])
     description = models.CharField(max_length=512, null=False, blank=False)
 
-    def __str__(self):
-        return self.user.first_name + ' ' + self.user.last_name
-
 
 class Article(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
@@ -32,6 +29,3 @@ class Article(models.Model):
 class Category(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
     cover = models.FileField(upload_to='files/category_cover', null=False, blank=False, validators=[validate_file_extension])
-
-    def __str__(self):
-        return self.title
